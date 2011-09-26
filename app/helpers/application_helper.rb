27c44ebff6b7da320f56765fc75ecdbf03052bb1
Colin Kuebler <kueblc@rpi.edu>
2011-09-25 23:12:41 -0400

    Removed useless logo definition Changed title-text base and separator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,13 @@
 module ApplicationHelper
 
-  def logo
-    logo = image_tag("logo.png", :alt => "Cheqit", :class => "round")
-  end
-
   #Return a title on a per-page basis.
   def title
-    base_title = "Cheqit"
+    base_title = "cheq.it"
+    separator = "-"
     if @title.nil?
       base_title
     else
-      "#{base_title} | #{@title}"
+      "#{base_title} #{separator} #{@title}"
     end
   end
 end
